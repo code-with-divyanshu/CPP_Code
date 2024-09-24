ebd4f58f21e93fbe6386e24b5194b568acb72617
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-09-24 23:54:58 +0530

    Start C++ Programing

---
+++ test/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+// using namespace std;
+
+// int main(){
+//    std::cout << "Hello Chai By Divyanshu" << std::endl;
+// }
+
+using std::cout;
+using std::endl;
+
+int main(){
+    cout << "Hello Chai By Divyanshu" << endl;
+    return 0; // means code exicuted successfully
+}
