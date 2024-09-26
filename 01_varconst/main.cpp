f8673db818b1f69eafe8a1280431cb75803ec2c0
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-09-26 19:45:48 +0530

    Learn Variables and Constant

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int score; // intialize variable
+    score = 110; // declare variable
+
+    int balance = 500; // in on line
+    balance = 1000; // variables, can change values
+
+    const int uid = 1232;
+    //  uid = 432; // can't change const variables
+
+    cout << "Welcome coders!" << endl;
+    return 0;
+}
