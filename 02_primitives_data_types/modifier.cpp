c2c5fe335cb227bc4f5221218175c0341808b588
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-30 17:36:23 +0530

    Modifier in Cpp

---
+++ 02_primitives_data_types/modifier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    unsigned int smallTeaPack = -100; // can't allow signed values
+
+    long largeTeaStorage =100000000;
+
+    short teaSample = 25;
+
+    cout << teaSample <<endl;
+
+    return 0;
+}
