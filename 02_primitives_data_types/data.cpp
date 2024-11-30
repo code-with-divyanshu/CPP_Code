928b347032eb7a0ece9b041b9768dbcbd609ae3f
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-30 17:35:58 +0530

    Primitive Data Types in Cpp

---
+++ 02_primitives_data_types/data.cpp
@@ -0,0 +1,16 @@
+//  Primitive Data Types in CPP
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int teaLeaves = 50;
+    float waterTemprature = 85.5;
+    double priceOfTea =  299.99;
+    char teaGrade = 'A';
+    bool isTeaReady = false;
+
+    cout << teaGrade << endl;
+    return 0;
+}
