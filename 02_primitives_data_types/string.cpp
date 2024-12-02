c6e7f77ef3d3c6ea69dda7b1a170563607eeb7ef
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-12-02 21:32:01 +0530

    strings in cpp

---
+++ 02_primitives_data_types/string.cpp
@@ -0,0 +1,16 @@
+// all thing about string
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string favoriteTea = "Phadi Chai";
+    string description = "Known as \"best\" tea";
+
+    cout << favoriteTea << endl;
+    cout << description << endl;
+
+    return 0;
+}
