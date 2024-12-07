4435749e77fa832f4402a2a872890d3189fcbed9
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-12-07 15:24:20 +0530

    Do While Loop in Cpp

---
+++ 05_loops/02_do_while_loop.cpp
@@ -0,0 +1,20 @@
+// do-while loop
+
+#include <iostream>
+#include <string>
+
+using namespace  std;
+
+int main() {
+    string response;
+
+
+    do
+    {
+        cout << "Do you want more tea (yes/no)";
+        getline(cin, response);
+    } while (response != "no" );
+    
+
+    return 0;
+}
