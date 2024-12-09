c1fd3d6fe791828fd5f6d2f4ada5b80a992019e4
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-12-09 23:49:02 +0530

    Pass array as a parameter in function

---
+++ 07_array/02_array.cpp
@@ -0,0 +1,17 @@
+// Basic of Array
+
+#include <iostream>
+
+using namespace  std;
+
+int main() {
+
+    int chaiServed[7] = {50, 60, 58, 72, 80, 94, 23};
+
+    cout << "Chai cups served on Day 1: " << chaiServed[0] << endl;
+    cout << "Chai cups served on Day 2: " << chaiServed[1] << endl;
+   
+    
+
+    return 0;
+}
