0bb43f11de0aa22510ccf6a209c3b55430099162
code-with-divyanshu <divyanshubartwal67@gmail.com>
2025-01-01 14:28:35 +0530

    Use Various Type of Algorithms and Iterations

--- 09_stl/employ_management_system.cpp
+++ 09_stl/employ_management_system.cpp
@@ -30,7 +30,11 @@ void displayEmployee(const Employee& emp) {
         {105, "Rahul", 50000},
     };
 
+    // Alogoritms 
+
     // lambda - unnamed functions start with squre bracket
+
+    // shorting data
 
     sort(employees.begin(), employees.end(), [](const Employee& e1, const Employee& e2){
         return e1.salary > e2.salary;
@@ -38,6 +42,42 @@ void displayEmployee(const Employee& emp) {
 
     cout << "Employees Sorted by Salary -> Highest to Lowest \n";
     for_each(employees.begin(), employees.end(), displayEmployee);
+
+
+    vector<Employee> highEarners;
+
+    // check condition
+
+    copy_if(employees.begin(),
+            employees.end(),
+            back_inserter(highEarners), 
+            [](const Employee& emp){
+            return emp.salary > 50000;
+    });
+
+    cout << "Employees Who are High earners\n";
+    for_each(highEarners.begin(), highEarners.end(), displayEmployee);
+
+    // get average salary of employees
+
+    double totalSalary = accumulate(employees.begin(), employees.end(), 0.0, [](double sum, const Employee& emp){
+        return sum + emp.salary;
+    });
+
+    double averageSalary = totalSalary / employees.size();
+
+    cout << "Employees Average Salary \n";
+    cout << averageSalary << endl;
+
+    //  check highest paid employee
+
+    auto highestPaid = max_element(employees.begin(), employees.end(), [](const Employee& emp1, const Employee& emp2){
+        return emp1.salary < emp2.salary;
+    });
+
+    cout << "Higest Paid Employee\n";
+    displayEmployee(*highestPaid);
+
 
     return 0;
  }
